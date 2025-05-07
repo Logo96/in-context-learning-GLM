b8725be296c53b76c953514fcf679e76591212ae
Joseph <jmtennyson7@gmail.com>
2025-05-07 06:50:33 +0000

    cleanup

--- src/train.py
+++ src/train.py
@@ -7,7 +7,6 @@ from tqdm import tqdm
 import torch
 import yaml
 
-from eval import get_run_metrics
 from tasks import get_task_sampler
 from samplers import get_data_sampler
 from curriculum import Curriculum
